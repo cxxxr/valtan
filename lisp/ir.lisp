62c6458aad0cb653f696c70175682cebea9dbdec
cxxxr <g23tlm@gmail.com>
2019-04-29 20:27:20 +0900

    change ir to struct

--- lisp/ir.lisp
+++ lisp/ir.lisp
@@ -1,15 +1,38 @@
 (in-package :compiler)
 
+(defstruct (ir (:constructor %make-ir))
+  op
+  return-value-p
+  multiple-values-p
+  arg1
+  arg2
+  arg3)
+
+(defmethod print-object ((ir ir) stream)
+  (cond ((ir-arg3 ir)
+         (format stream "(~S ~S ~S ~S)" (ir-op ir) (ir-arg1 ir) (ir-arg2 ir) (ir-arg3 ir)))
+        ((ir-arg2 ir)
+         (format stream "(~S ~S ~S)" (ir-op ir) (ir-arg1 ir) (ir-arg2 ir)))
+        ((ir-arg1 ir)
+         (format stream "(~S ~S)" (ir-op ir) (ir-arg1 ir)))))
+
 (defun make-ir (op return-value-p multiple-values-p &rest args)
   (ecase (length args)
-    (1 (vector op return-value-p multiple-values-p (first args)))
-    (2 (vector op return-value-p multiple-values-p (first args) (second args)))
-    (3 (vector op return-value-p multiple-values-p (first args) (second args) (third args)))))
-
-(defun ir-op (ir) (aref ir 0))
-(defun ir-arg1 (ir) (aref ir 1))
-(defun ir-arg2 (ir) (aref ir 2))
-(defun ir-arg3 (ir) (aref ir 3))
+    (1 (%make-ir :op op
+                 :return-value-p return-value-p
+                 :multiple-values-p multiple-values-p
+                 :arg1 (first args)))
+    (2 (%make-ir  :op op
+                  :return-value-p return-value-p
+                  :multiple-values-p multiple-values-p
+                  :arg1 (first args)
+                  :arg2 (second args)))
+    (3 (%make-ir :op op
+                 :return-value-p return-value-p
+                 :multiple-values-p multiple-values-p
+                 :arg1 (first args)
+                 :arg2 (second args)
+                 :arg3 (third args)))))
 
 (defstruct parsed-lambda-list
   vars
