5b8a6e874d21c2db463f828a811eaa957291af75
cxxxr <g23tlm@gmail.com>
2019-05-23 23:18:47 +0900

    tweak

--- lisp/string.lisp
+++ lisp/string.lisp
@@ -1,8 +1,5 @@
 (in-package :common-lisp)
 
-(defun stringp (x)
-  (equal (ffi:typeof x) "string"))
-
 (defun string (x)
   (cond ((characterp x)
          ((ffi:ref "String" "fromCharCode") (char-code x)))
