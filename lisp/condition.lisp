49e6807f0193bea0a2d19f71833ddb0385bb468c
cxxxr <g23tlm@gmail.com>
2019-05-26 01:42:18 +0900

    add assert

--- lisp/condition.lisp
+++ lisp/condition.lisp
@@ -3,3 +3,8 @@
 (defun error (datum &rest arguments)
   ;(system:%error (format nil datum arguments))
   (system:%error (array-contents datum)))
+
+(defmacro assert (test-form &optional place datum-form argument-form)
+  (declare (ignore place datum-form argument-form))
+  `(unless ,test-form
+     (error ,(format nil "assertion error: ~S" test-form))))
