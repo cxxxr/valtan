a225921fb30b562b282b38be08388397b9903987
cxxxr <g23tlm@gmail.com>
2019-06-08 22:20:39 +0900

    arrayの整備

--- lisp/array.lisp
+++ lisp/array.lisp
@@ -17,17 +17,45 @@
           (setq total-size (* total-size d)))
         total-size)))
 
-(defun make-array-contents (size element-type initial-element)
+(defun check-dimensions-and-initial-contents (dimensions initial-contents)
+  (let ((initial-contents-length (length initial-contents)))
+    (unless (= dimensions
+               initial-contents-length)
+      (error "error")
+      #+(or)
+      (error "There are ~D elements in the :INITIAL-CONTENTS, but the vector length is ~D."
+             initial-contents-length
+             dimensions))))
+
+(defun make-array-contents-with-initial-contents (size element-type initial-contents)
+  (cond ((eq element-type 'character)
+         (error "trap"))
+        (t
+         (let ((js-array (ffi:new (ffi:ref "Array") size)))
+           (do ((i 0 (1+ i))
+                (initial-contents* initial-contents (cdr initial-contents*)))
+               ((null initial-contents*))
+             (ffi:set (ffi:index js-array i) (car initial-contents*)))
+           js-array))))
+
+(defun make-array-contents-with-initial-element (size element-type initial-element initial-element-p)
   (cond ((eq element-type 'character)
          ((ffi:ref ((ffi:ref "String" "fromCharCode")
-                    (char-code initial-element))
+                    (if initial-element-p
+                        (char-code initial-element)
+                        0))
                    "repeat")
           size))
         (t
          ((ffi:ref (ffi:new (ffi:ref "Array") size) "fill") initial-element))))
 
-(defun make-array (dimensions &key element-type initial-element initial-contents adjustable
-                                   fill-pointer displaced-to displaced-index-offset)
+(defun make-array (dimensions &key element-type
+                                   (initial-element nil initial-element-p)
+                                   (initial-contents nil initial-contents-p)
+                                   adjustable
+                                   fill-pointer
+                                   displaced-to
+                                   displaced-index-offset)
   (unless (integerp dimensions)
     (error "error"))
   (when (and (listp dimensions)
@@ -39,7 +67,18 @@
                    (<= 0 fill-pointer)))
     ;(error "Bad fill-pointer: ~S" fill-pointer)
     (error "Bad fill-pointer"))
-  (let ((contents (make-array-contents dimensions element-type initial-element)))
+  (cond ((and initial-contents-p initial-element-p)
+         (error "Can't specify both :INITIAL-ELEMENT and :INITIAL-CONTENTS"))
+        (initial-contents-p
+         (check-dimensions-and-initial-contents dimensions initial-contents)))
+  (let ((contents (if initial-contents-p
+                      (make-array-contents-with-initial-contents dimensions
+                                                                 element-type
+                                                                 initial-contents)
+                      (make-array-contents-with-initial-element dimensions
+                                                                element-type
+                                                                initial-element
+                                                                initial-element-p))))
     (%make-array :contents contents
                  :fill-pointer fill-pointer
                  :rank (if (listp dimensions)
@@ -59,14 +98,27 @@
     (error "type error"))
   (when (or (< sub 0) (<= (array-length array) sub))
     (error "index error"))
-  (ffi:index (array-contents array) sub))
+  (cond ((eq (array-element-type array) 'character)
+         (ffi:index (array-contents array) sub))
+        (t
+         (ffi:index (array-contents array) sub))))
 
 (defun (setf aref) (value array sub)
   (unless (arrayp array)
     (error "type error"))
   (when (or (< sub 0) (<= (array-length array) sub))
     (error "index error"))
-  (ffi:set (ffi:index (array-contents array) sub) value))
+  (cond ((eq (array-element-type array) 'character)
+         (unless (characterp value)
+           (error "type error"))
+         (setf (array-contents array)
+               ((ffi:ref ((ffi:ref (array-contents array) "substring") 0 sub)
+                         "concat")
+                value
+                ((ffi:ref (array-contents array) "substring") (1+ sub))))
+         value)
+        (t
+         (ffi:set (ffi:index (array-contents array) sub) value))))
 
 (defun vectorp (x)
   (and (arrayp x) (= 1 (array-rank x))))
