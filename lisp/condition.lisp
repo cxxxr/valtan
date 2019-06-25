8a99540444485278266faac9d965fc5f5a9e74d9
cxxxr <g23tlm@gmail.com>
2019-06-25 19:05:36 +0900

    type-error

--- lisp/condition.lisp
+++ lisp/condition.lisp
@@ -1,10 +1,12 @@
 (in-package :common-lisp)
 
 (defun error (datum &rest arguments)
-  ;(system:%error (format nil datum arguments))
-  (system::error (system::array-to-js-string datum)))
+  (system::error (system::array-to-js-string (apply #'format nil datum arguments))))
 
 (defmacro assert (test-form &optional place datum-form argument-form)
   (declare (ignore place datum-form argument-form))
   `(unless ,test-form
      (error ,(format nil "assertion error: ~S" test-form))))
+
+(defun type-error (value type-name)
+  (error "The value ~S is not of the expected type ~A" value type-name))
