085f9d25bbf2d26e663d9a37f23f876372781940
cxxxr <g23tlm@gmail.com>
2019-06-29 16:13:00 +0900

    js-evalを追加

--- lisp/ffi.lisp
+++ lisp/ffi.lisp
@@ -8,3 +8,8 @@
 
 (defmacro ffi:console.log (&rest args)
   `((ffi:ref "console" "log") ,@args))
+
+(defun ffi::js-eval (x)
+  (let* ((code (format nil "(function(lisp) { 'use strict'; ~A; });" x))
+         (fn (ffi::%js-eval (system::array-to-js-string code))))
+    (funcall fn (ffi:ref "lisp"))))
