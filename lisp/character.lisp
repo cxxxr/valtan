6c706ad29c9ec8c4d22db780efd211e6e6eb4de6
cxxxr <g23tlm@gmail.com>
2019-07-07 18:18:49 +0900

    char-upcase,char-downcaseを追加

--- lisp/character.lisp
+++ lisp/character.lisp
@@ -1,1 +1,13 @@
 (in-package :common-lisp)
+
+(defun char-downcase (char)
+  (if (char<= #\A char #\Z)
+      (code-char (+ (- (char-code char) (char-code #\A))
+                    (char-code #\a)))
+      char))
+
+(defun char-upcase (char)
+  (if (char<= #\a char #\z)
+      (code-char (+ (- (char-code char) (char-code #\a))
+                    (char-code #\A)))
+      char))
