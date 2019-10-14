9585cc4e0372aef286ec0c13c300d5a15bdcacf4
cxxxr <g23tlm@gmail.com>
2019-10-14 18:00:07 +0900

    tweak

--- example/tests.lisp
+++ example/tests.lisp
@@ -7,9 +7,16 @@
           ((eq form eof-value))
         (prin1 form)
         (terpri)
-        (assert (eval form))))))
+        (handler-case
+            (eval form)
+          (error (e)
+            (format t "*** ERROR ***: ~A~%" e))
+          (:no-error (ok)
+            (if ok
+                (write-line "OK")
+                (write-line "FAIL"))))))))
 
-(test "example/sacla-tests/must-cons.lisp")
-(test "example/sacla-tests/must-character.lisp")
-(test "example/sacla-tests/must-string.lisp")
-;; (test "example/sacla-tests/must-sequence.lisp")
+;; (test "example/sacla-tests/must-cons.lisp")
+;; (test "example/sacla-tests/must-character.lisp")
+;; (test "example/sacla-tests/must-string.lisp")
+(test "example/sacla-tests/must-sequence.lisp")
