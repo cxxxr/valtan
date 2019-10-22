acd79cfd742d41d75ffad3c0350618759b8499cf
cxxxr <g23tlm@gmail.com>
2019-10-22 19:57:14 +0900

    remove unused file

--- cl-loader/index.js
+++
@@ -1,6 +0,0 @@
-const { execFileSync } = require('child_process');
-
-module.exports = function(source) {
-  const output = execFileSync('valtan', [], {input: source});
-  return output.toString();
-}
