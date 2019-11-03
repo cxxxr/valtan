2b329fef50582fe27242fc2d9cac296a9df87a32
cxxxr <g23tlm@gmail.com>
2019-11-03 15:05:36 +0900

    tweak

--- example/profile/main.lisp
+++ example/profile/main.lisp
@@ -1,2 +1,17 @@
-((ffi:ref "lisp" "finishProfile"))
+;; (defmacro time (form)
+;;   (let ((start (gensym)))
+;;     `(let ((,start (js:-date.now)))
+;;        ,form
+;;        (format t "~&time: ~A~%" (- (js:-date.now) ,start)))))
 
+;; (time (eval '(defun fact (n)
+;;               (if (zerop n)
+;;                   1
+;;                   (* n (fact (1- n)))))))
+
+(eval '(defun fact (n)
+        (if (zerop n)
+            1
+            (* n (fact (1- n))))))
+
+((ffi:ref "lisp" "finishProfile"))
