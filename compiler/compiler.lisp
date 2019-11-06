c514d0de9996d59481fe63112522008ede369739
cxxxr <g23tlm@gmail.com>
2019-11-06 18:56:11 +0900

    optimize

--- compiler/compiler.lisp
+++ compiler/compiler.lisp
@@ -16,20 +16,36 @@
 #+(or)
 (let ((file (asdf:system-relative-pathname :valtan "./lib/lisp.js")))
   (with-open-file (in file)
-    (loop :for line := (read-line in nil nil)
-          :while line
-          :when (ppcre:register-groups-bind (package-name symbol-name fn-name)
-                    ("^registerFunction\\((\\w+),\\s*'([^']*)',\\s*(\\w+)," line)
-                  (list (format nil "lisp.~A" fn-name)
-                        (format nil "~A:~A"
-                                (cond ((string= package-name "cl_package")
-                                       "CL:")
-                                      ((string= package-name "system_package")
-                                       "SYSTEM::")
-                                      ((string= package-name "ffi_package")
-                                       "FFI::"))
-                                symbol-name)))
-          :collect :it)))
+    (let ((*print-case* :downcase))
+      (pprint
+       `(defparameter compiler::*builtin-function-table*
+          (let ((table (make-hash-table)))
+            ,@(loop :for line := (read-line in nil nil)
+                    :while line
+                    :when (ppcre:register-groups-bind (package-name symbol-name fn-name args)
+                              ("^registerFunction\\((\\w+),\\s*'([^']*)',\\s*(\\w+),\\s*(.*)"
+                               line)
+                            `(setf (gethash
+                                    (read-from-string
+                                     ,(format nil "~A:~A"
+                                              (cond ((string= package-name "cl_package")
+                                                     "CL")
+                                                    ((string= package-name "system_package")
+                                                     "SYSTEM:")
+                                                    ((string= package-name "ffi_package")
+                                                     "FFI:"))
+                                              symbol-name))
+                                    table)
+                                   (list ,(format nil "lisp.~A" fn-name)
+                                         ,(let ((tokens '()))
+                                            (ppcre:do-register-groups (token) ("(\\w+)" args)
+                                              (push (if (string= token "null")
+                                                        nil
+                                                        (parse-integer token))
+                                                    tokens))
+                                            `(list ,@(nreverse tokens))))))
+                    :collect :it)
+            table))))))
 
 (defvar *js-readtable*
   (let ((*readtable* (copy-readtable)))
