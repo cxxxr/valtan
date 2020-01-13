8161bb0912dc9ccca900298442f381cb6cc36aed
cxxxr <g23tlm@gmail.com>
2020-01-13 15:19:20 +0900

    add browser-repl

--- example/browser-repl/main.lisp
+++ example/browser-repl/main.lisp
@@ -1,73 +1,71 @@
+(ffi:require js:react "react")
+(ffi:require js:react-dom "react-dom")
+(ffi:require js:-code-mirror "react-codemirror2")
+(ffi:require "codemirror/keymap/emacs")
+(ffi:require "codemirror/mode/commonlisp/commonlisp")
+
 #|
-(defun map-index (type fn sequence)
-  (let ((i -1))
-    (map type
-         (lambda (x)
-           (funcall fn x (incf i)))
-         sequence)))
-
-(define-react-component js:-history (history)
-  (tag :ul ()
-       (map-index 'vector
-                  (lambda (text i)
-                    (tag :li (:key i) text))
-                  history)))
-
-(define-react-component js:-input (on-return)
-  (with-state ((text set-text ""))
-    (tag :input (:option #j"text"
-                 :value (ffi:cl->js text)
-                 :on-change (lambda (e)
-                              (set-text (ffi:js->cl (ffi:ref e "target" "value"))))
-                 :on-key-press (lambda (e)
-                                 (when (eq (ffi:ref e "key") #j"Enter")
-                                   ((ffi:ref e "preventDefault"))
-                                   (set-text #j"")
-                                   (funcall on-return text)))))))
-
-(define-react-component js:-listener ()
-  (with-state ((result set-result nil)
-               (history set-history nil))
-    (tag :div (:style (ffi:object :display #j"flex" :flex-Direction #j"column"))
-         (tag js:-history (:history history))
-         (tag js:-input (:on-return (lambda (text)
-                                      (set-history
-                                       (append history
-                                               (list text
-                                                     (eval (read-from-string text)))))))))))
-
-(define-react-component js:-app ()
-  (tag js:-listener ()))
-
-(unless (eq (ffi:typeof js:window) #j"undefined")
-  (js:react-dom.render
-   (js:react.create-element js:-app)
-   (js:document.get-element-by-id #j"repl")))
+TODO
+- *package*の値がおかしい
+- in-packageが動かない
+- (cons 1 2 3)でエラーハンドリングできてなさそう
+- スタックトレースを出す
+- REPLに色を付ける
 |#
 
-(defun send-eval (e)
-  (let ((text (ffi:js->cl ((ffi:ref *code-mirror* :get-value)))))
-    (js:alert (ffi:cl->js (eval (read-from-string text))))))
+(defvar *repl-package* (find-package :valtan-user))
 
-(defparameter *code-mirror*
-  (js:-code-mirror
-   (js:document.get-element-by-id #j"repl")
-   (ffi:object :mode #j"commonlisp"
-               :key-map #j"emacs"
-               :extra-keys (ffi:object #j"Enter" #'send-eval))))
+(defun send-eval (e append-lines)
+  (let* ((*package* *repl-package*)
+         (text (ffi:js->cl ((ffi:ref e :get-value))))
+         (incomplete '#:incomplete)
+         (form
+           (handler-case (read-from-string text)
+             (error () incomplete)))
+         (old-package *repl-package*))
+    (if (eq form incomplete)
+        ((ffi:ref e :replace-selection) #j(string #\newline) #j"end")
+        (let ((value (handler-case (format nil "~S" (eval form))
+                       (error (e) (princ-to-string e)))))
+          (funcall append-lines
+                   (list (format nil "~A> ~A" (package-name old-package) text)
+                         value))
+          ((ffi:ref e :set-value) #j"")))))
 
-((ffi:ref *code-mirror* "focus"))
+(define-react-component js:-backlog (lines)
+  (tag :ul (:class-name "back-log")
+       (let ((i 0))
+         (map 'vector
+              (lambda (line)
+                (tag :li (:key (incf i))
+                     (tag :span (:class-name "prompt")
+                          line)))
+              lines))))
 
-(let ((last-length 0))
-  (labels ((handle-change (&rest args)
-             (declare (ignore args))
-             (let* ((text (ffi:js->cl ((ffi:ref *code-mirror* :get-value))))
-                    (length (length text)))
-               (when (and (< last-length length)
-                          (ignore-errors (read-from-string text) t))
-                 ((ffi:ref *code-mirror* :off) #j"change" #'handle-change)
-                 (handle-enter text)
-                 ((ffi:ref *code-mirror* :on) #j"change" #'handle-change))
-               (setq last-length length))))
-    ;; ((ffi:ref *code-mirror* :on) #j"change" #'handle-change)
-    ((ffi:ref *code-mirror* "focus"))))
+(define-react-component js:-repl ()
+  (with-state ((lines set-lines nil))
+    (tag :div ()
+         (tag js:-backlog (:lines lines))
+         (tag :div (:class-name "repl-input")
+              (tag :span (:class-name "prompt") (format nil "~A>" (package-name *repl-package*)))
+              (tag (ffi:ref js:-code-mirror :-un-controlled)
+                   (:value ""
+                    :options (ffi:object
+                              :mode "commonlisp"
+                              :key-map "emacs"
+                              :extra-keys (ffi:object
+                                           :-enter (lambda (e)
+                                                     (send-eval e
+                                                                (lambda (new-lines)
+                                                                  (set-lines
+                                                                   (append
+                                                                    lines
+                                                                    new-lines))))))
+                              :autofocus js:true)
+                    :editor-did-mount (lambda (editor &rest args)
+                                        (declare (ignore args))
+                                        ((ffi:ref editor :focus)))))))))
+
+(js:react-dom.render
+ (js:react.create-element js:-repl)
+ (js:document.get-element-by-id #j"repl"))
