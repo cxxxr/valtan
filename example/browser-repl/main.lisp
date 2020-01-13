95da3dd35271a7dff12eadad7b0d36a442a73b5f
cxxxr <g23tlm@gmail.com>
2020-01-14 00:49:24 +0900

    update

--- example/browser-repl/main.lisp
+++ example/browser-repl/main.lisp
@@ -21,17 +21,20 @@ TODO
            (form
              (handler-case (read-from-string text)
                (error () incomplete))))
-      (js:console.log (ffi:cl->js (package-name *package*)))
       (if (eq form incomplete)
           ((ffi:ref e :replace-selection) #j(string #\newline) #j"end")
           (let ((value (handler-case (format nil "~S" (eval form))
                          (error (e) (princ-to-string e)))))
             (funcall append-lines
-                     (list (format nil "~A> ~A" (package-name repl-package) text)
+                     (list (list (package-name repl-package) text)
                            value))
             ((ffi:ref e :set-value) #j"")))
-      (js:console.log (ffi:cl->js (package-name *package*)))
       (funcall set-repl-package *package*))))
+
+(define-react-component js:-prompt (package-name)
+  ;; (tag js:-prompt (:package-name ...))で受け取ったときにはstringがcl->jsされているので一旦元に戻さないといけない
+  (setq package-name (ffi:js->cl package-name))
+  (tag :span (:class-name "prompt") (format nil "~A>" package-name)))
 
 (define-react-component js:-backlog (lines)
   (tag :ul (:class-name "back-log")
@@ -39,8 +42,13 @@ TODO
          (map 'vector
               (lambda (line)
                 (tag :li (:key (incf i))
-                     (tag :span (:class-name "prompt")
-                          line)))
+                     (if (consp line)
+                         (destructuring-bind (package-name code) line
+                           (tag :div (:class-name "line")
+                                (tag js:-prompt (:package-name package-name))
+                                (tag :span (:class-name "code") code)))
+                         (tag :span (:class-name "code")
+                              line))))
               lines))))
 
 (define-react-component js:-repl ()
@@ -49,7 +57,7 @@ TODO
     (tag :div ()
          (tag js:-backlog (:lines lines))
          (tag :div (:class-name "repl-input")
-              (tag :span (:class-name "prompt") (format nil "~A>" (package-name repl-package)))
+              (tag js:-prompt (:package-name (package-name repl-package)))
               (tag (ffi:ref js:-code-mirror :-un-controlled)
                    (:value ""
                     :options (ffi:object
