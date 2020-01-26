05c6b2fa29ac318578881551886675b7468d3951
cxxxr <g23tlm@gmail.com>
2020-01-26 18:50:15 +0900

    fix

--- skeleton/browser-skeleton/main.lisp
+++ skeleton/browser-skeleton/main.lisp
@@ -1,5 +1,8 @@
 (ffi:require js:react "react")
 (ffi:require js:react-dom "react-dom")
+(defpackage :<% @var name %>
+  (:use :cl :valtan.react-utilities))
+(in-package :<% @var name %>)
 
 (define-react-component js:-app ()
   (tag :h1 () "hello world"))
