1cf1cc649531160ea4f7d6e7cac4aa798d73caae
cxxxr <g23tlm@gmail.com>
2020-01-26 22:39:40 +0900

    fix

--- skeleton/browser-skeleton/main.lisp
+++ skeleton/browser-skeleton/main.lisp
@@ -5,9 +5,7 @@
   (:use :cl :valtan.react-utilities))
 (in-package :<% @var name %>)
 
-(define-react-component js:-app ()
+(define-react-component <app> ()
   (tag :h1 () "hello world"))
 
-(js:react-dom.render
- (js:react.create-element js:-app)
- (js:document.get-element-by-id #j"root"))
+(setup #'<app> "root")
