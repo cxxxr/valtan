1b4dea04fee340ae6965584f9815df8b095543ad
cxxxr <g23tlm@gmail.com>
2020-03-22 17:39:30 +0900

    add valtan-core-tests

---
+++ library/valtan-core/tests/tests.lisp
@@ -0,0 +1,33 @@
+(cl:defpackage :valtan-core/tests
+  (:use :cl))
+(cl:in-package :valtan-core/tests)
+
+(defvar *valtan-package* (find-package :valtan-core))
+
+(defun test-pathname (name)
+  (make-pathname :name name
+                 :type "lisp"
+                 :defaults (asdf:system-relative-pathname :valtan-core "tests/sacla-tests/")))
+
+(defun test-form (form)
+  (pprint form)
+  (unless (eval form)
+    (cerror "NG" "~S" form)))
+
+(defmacro with-valtan-env (() &body body)
+  `(let ((*package* *valtan-package*)
+         (*readtable* valtan-core::*valtan-readtable*))
+     ,@body))
+
+(defun run-test (pathname)
+  (with-open-file (in pathname)
+    (loop :with eof := '#:eof
+          :for form := (with-valtan-env ()
+                         (read in nil eof))
+          :until (eq form eof)
+          :do (with-valtan-env ()
+                (test-form form)))))
+
+(defun main ()
+  (run-test (test-pathname "must-cons")))
+
---
+++ library/valtan-core/valtan-core-tests.asd
@@ -0,0 +1,5 @@
+(defsystem "valtan-core-tests"
+  :depends-on ("valtan-core")
+  :serial t
+  :components ((:file "tests/tests")))
+
