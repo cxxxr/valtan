58ea5ad836adfacad57d3e47e01129f4d181ebad
cxxxr <g23tlm@gmail.com>
2020-03-29 23:07:58 +0900

    indent

--- library/valtan-core/lisp/host-literal.lisp
+++ library/valtan-core/lisp/host-literal.lisp
@@ -7,58 +7,63 @@
   (cl:declare (cl:ignore environment))
   (cl:make-load-form-saving-slots object))
 
-(cl:set-macro-character #\" (cl:lambda (s c)
-                              (cl:declare (cl:ignore c))
-                              (cl:unread-char #\" s)
-                              (cl:let* ((cl:*readtable* *plain-readtable*)
-                                        (raw-string (cl:read s cl:t cl:nil cl:t)))
-                                (system:make-structure 'array
-                                                       raw-string
-                                                       nil
-                                                       1
-                                                       (cl:length raw-string)
-                                                       'character)))
-                        nil
-                        *valtan-readtable*)
+(cl:set-macro-character
+ #\"
+ (cl:lambda (s c)
+   (cl:declare (cl:ignore c))
+   (cl:unread-char #\" s)
+   (cl:let* ((cl:*readtable* *plain-readtable*)
+             (raw-string (cl:read s cl:t cl:nil cl:t)))
+     (system:make-structure 'array
+                            raw-string
+                            nil
+                            1
+                            (cl:length raw-string)
+                            'character)))
+ nil
+ *valtan-readtable*)
 
-(cl:set-dispatch-macro-character #\# #\(
-                                 (cl:lambda (s c n)
-                                   (cl:declare (cl:ignore c n))
-                                   (cl:unread-char #\( s)
-                                   (cl:let* ((cl:*readtable* *plain-readtable*)
-                                             (raw-array (cl:read s cl:t cl:nil cl:t)))
-                                     (system:make-structure 'array
-                                                            (cl:coerce raw-array 'cl:vector)
-                                                            nil
-                                                            1
-                                                            (cl:length raw-array)
-                                                            't)))
-                                 *valtan-readtable*)
+(cl:set-dispatch-macro-character
+ #\# #\(
+ (cl:lambda (s c n)
+   (cl:declare (cl:ignore c n))
+   (cl:unread-char #\( s)
+   (cl:let* ((cl:*readtable* *plain-readtable*)
+             (raw-array (cl:read s cl:t cl:nil cl:t)))
+     (system:make-structure 'array
+                            (cl:coerce raw-array 'cl:vector)
+                            nil
+                            1
+                            (cl:length raw-array)
+                            't)))
+ *valtan-readtable*)
 
-(cl:set-dispatch-macro-character #\# #\*
-                                 (cl:lambda (s c n)
-                                   (cl:declare (cl:ignore c n))
-                                   (cl:let* ((cl:*readtable* *plain-readtable*)
-                                             (bits (cl:coerce
-                                                    (cl:loop
-                                                      :while (cl:member (cl:peek-char cl:nil s cl:t cl:nil cl:t)
-                                                                        '(#\0 #\1))
-                                                      :collect (cl:ecase (cl:read-char s cl:t cl:nil cl:t)
-                                                                 (#\0 0)
-                                                                 (#\1 1)))
-                                                    'cl:vector)))
-                                     (system:make-structure 'array
-                                                            bits
-                                                            nil
-                                                            1
-                                                            (cl:length bits)
-                                                            't)))
-                                 *valtan-readtable*)
+(cl:set-dispatch-macro-character
+ #\# #\*
+ (cl:lambda (s c n)
+   (cl:declare (cl:ignore c n))
+   (cl:let* ((cl:*readtable* *plain-readtable*)
+             (bits (cl:coerce
+                    (cl:loop
+                      :while (cl:member (cl:peek-char cl:nil s cl:t cl:nil cl:t)
+                                        '(#\0 #\1))
+                      :collect (cl:ecase (cl:read-char s cl:t cl:nil cl:t)
+                                 (#\0 0)
+                                 (#\1 1)))
+                    'cl:vector)))
+     (system:make-structure 'array
+                            bits
+                            nil
+                            1
+                            (cl:length bits)
+                            't)))
+ *valtan-readtable*)
 
-(cl:set-dispatch-macro-character #\# #\"
-                                 (cl:lambda (s c n)
-                                   (cl:declare (cl:ignore c n))
-                                   (cl:unread-char #\" s)
-                                   (let ((cl:*readtable* *plain-readtable*))
-                                     (cl:read s cl:t cl:nil cl:t)))
-                                 *valtan-readtable*)
+(cl:set-dispatch-macro-character
+ #\# #\"
+ (cl:lambda (s c n)
+   (cl:declare (cl:ignore c n))
+   (cl:unread-char #\" s)
+   (let ((cl:*readtable* *plain-readtable*))
+     (cl:read s cl:t cl:nil cl:t)))
+ *valtan-readtable*)
