94c8cfdaea07f132950608099d2b3a50dcb4e8f9
cxxxr <g23tlm@gmail.com>
2020-04-12 20:01:27 +0900

    fix

--- library/valtan-core/compiler/interface.lisp
+++ library/valtan-core/compiler/interface.lisp
@@ -2,12 +2,18 @@
 
 (defvar *compiler-interfaces* '())
 
-(defmacro def-interface (name lambda-list &body body)
+(defmacro def-interface (name args &body body)
   `(progn
      (pushnew ',name *compiler-interfaces*)
-     (defun ,name ,lambda-list ,@body)))
+     (setf (get ',name 'implementation)
+           (lambda ,args ,@body))
+     (defun ,name ,args
+       (funcall (get ',name 'implementation)
+                ,@args))))
 
-(defmacro def-implementation (name lambda-list &body body)
+(defmacro def-implementation (name args &body body)
   `(progn
      (assert (member ',name *compiler-interfaces*))
-     (defun ,name ,lambda-list ,@body)))
+     (setf (get ',name 'implementation)
+           (flet ((,name ,args ,@body))
+             #',name))))
